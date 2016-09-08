a12068a921c29c7d365ecf6db59e694be6e5a887
Alan Lewis <alan.lewis@objectaction.com>
2016-09-08 12:10:37 +0100

    Add  discount and additional tests as step 2.

---
+++ src/main/scala/ProduceOffers.scala
@@ -0,0 +1,18 @@
+
+import scala.collection.mutable.ListBuffer
+
+trait ProduceOffers {
+  
+  // buy 1 get 1 free
+  def appleDiscount(basket: ListBuffer[String]): Int = {
+    val count = basket.count(_ == "Apple")
+    if (count > 1)  count / 2  else 0
+  }
+  
+  // buy 3 get 1 free
+  def orangeDiscount(basket: ListBuffer[String]): Int = {
+    val count = basket.count(_ == "Orange")
+    if(count >= 3) count / 3 else 0
+  }
+  
+}
